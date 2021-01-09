8b59972ef31abf8f7fa3f63a5848cff48acd210e
hegemon76 <adrian.dawid76@gmail.com>
2021-01-09 22:03:08 +0100

    add user registration add base controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register (string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+           _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return user;
+            
+        }
+
+    }
+}
