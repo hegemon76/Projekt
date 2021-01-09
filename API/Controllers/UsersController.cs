c2ffdd6a7cb57889a532c7bb949e3d96e4b61773
hegemon76 <adrian.dawid76@gmail.com>
2021-01-09 18:29:45 +0100

    add async get users add async get specified user

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _contex;
+
+        public UsersController(DataContext contex)
+        {
+            _contex = contex;
+        }
+
+
+        // api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _contex.Users.ToListAsync();
+        }
+
+        // api/users/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return _contex.Users.Find(id);
+        }
+    }
+}
