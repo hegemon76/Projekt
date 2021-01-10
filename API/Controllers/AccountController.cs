a358dac249cc3750856c4e5b68986adcedf14e07
hegemon76 <adrian.dawid76@gmail.com>
2021-01-10 12:31:09 +0100

    add login logic

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -25,7 +25,7 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
-            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+            if (await UserExists(registerDto.Username)) return BadRequest("Nazwa użytkownika jest zajęta");
 
             using var hmac = new HMACSHA512();
 
@@ -42,6 +42,26 @@ namespace API.Controllers
 
             return user;
 
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Nieprawidłowa nazwa użytkownika");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Nieprawidłowe hasło");
+            }
+
+            return user;
         }
 
         private async Task<bool> UserExists(string username)
